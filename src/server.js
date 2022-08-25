d411e8de41ce71296af4b721e7eac141f183d777
Mikuláš Vojíř <vojir.mikulas@gmail.com>
2022-08-26 00:34:16 +0200

    xdd

--- src/server.js
+++ src/server.js
@@ -2,9 +2,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let pdf = require("html-pdf")
+app.use((req, res, next) => {
 
+    if (req.originalUrl === '/stripe/webhook') {
+        next();
+    } else {
+        express.json()(req, res, next);
+    }
+});
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+app.get('/pdf',(req,res)=>{
+    let options = { format: 'Letter' };
+    let html = "<h1>TVOJE MÁMA</h1>"
+
+    pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+        if (err) return console.log(err);
+        console.log(res); // { filename: '/app/businesscard.pdf' }
+    });
 })
 
 app.listen(port, () => {
